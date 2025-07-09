0c141bf1fa04badb08756ce197030f28c945c58f
mariotoscano1997 <toscanomario97@gmail.com>
2025-07-10 01:01:24 +0200

    change 'cpu' mode for sahi

--- script/predict_for_gui.py
+++ script/predict_for_gui.py
@@ -19,7 +19,7 @@ def sahi_prediction(model_path, frame, slice_height = 640, slice_width = 640):
         model_type="ultralytics",
         model_path=model_path,
         confidence_threshold=CONF,
-        device=0
+        device='cpu'
     )
     results = get_sliced_prediction(frame, detection_model,overlap_height_ratio=0.2,overlap_width_ratio=0.2, slice_height =slice_height,slice_width = slice_width ,perform_standard_pred=False)
     detection_data = [
